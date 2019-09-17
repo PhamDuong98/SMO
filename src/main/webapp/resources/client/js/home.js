1ac4afd26004935b372463930a4d443a0bbb6947
PhamDuong98 <duongpmse05785@fpt.edu.vn>
2019-09-17 20:50:07 +0700

    update

--- src/main/webapp/resources/client/js/home.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-		$("#header").load("view/client/header.html");
-		$("#scroll").load("view/client/scroll.html");
-		$("#footer").load("view/client/footer.html");
-});
